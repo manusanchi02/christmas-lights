4ac5190ef958c536811c2226f98d8021e974dcba
Emanuele Sanchi <emanuele.sanchi@studio.unibo.it>
2025-12-01 10:42:18 +0100

    feat: add Morse code mode for LED control and update UI

--- app.py
+++ app.py
@@ -4,35 +4,50 @@ from time import sleep
 import threading
 from enum import Enum
 
+
 class Mode(Enum):
     FIXED = 0
     FADE = 1
     SHOW = 2
-    
+    MORSE = 3
+
+
 mode = Mode.FIXED
 
-# Configurazione del LED
+# Led config
+GPIO.setmode(GPIO.BCM)
 ledPin = 21
 pwm_led = PWMLED(ledPin, active_high=True, initial_value=0, frequency=50)
 
-# Creazione dell'app Flask
+morse_dict = {
+    'A': '.-', 'B': '-...', 'C': '-.-.', 'D': '-..', 'E': '.', 'F': '..-.',
+                    'G': '--.', 'H': '....', 'I': '..', 'J': '.---', 'K': '-.-', 'L': '.-..',
+                    'M': '--', 'N': '-.', 'O': '---', 'P': '.--.', 'Q': '--.-', 'R': '.-.',
+                    'S': '...', 'T': '-', 'U': '..-', 'V': '...-', 'W': '.--', 'X': '-..-',
+                    'Y': '-.--', 'Z': '--..', ' ': '/'
+}
+
 app = Flask(__name__)
+
 
 def shutdown_server():
     func = request.environ.get('werkzeug.server.shutdown')
     if func is None:
         raise RuntimeError('Not running with the Werkzeug Server')
     func()
-    
+
+
 @app.route('/shutdown', methods=['POST'])
 def shutdown():
     shutdown_server()
     return 'Server shutting down...'
 
+
 @app.route('/')
 def index():
     global mode
     return render_template('index.html', mode=mode)
+
 
 @app.route('/toggle_fade', methods=['POST'])
 def toggle_fade():
@@ -40,19 +55,28 @@ def toggle_fade():
     mode = Mode.FADE
     return redirect(url_for('index', mode=mode))
 
+
 @app.route('/toggle_fixed', methods=['POST'])
 def toggle_fixed():
     global mode
     mode = Mode.FIXED
     return redirect(url_for('index', mode=mode))
 
+
 @app.route('/toggle_show', methods=['POST'])
 def toggle_show():
     global mode
     mode = Mode.SHOW
     return redirect(url_for('index', mode=mode))
- 
-# Funzione per il controllo del LED
+
+
+@app.route('/toggle_morse', methods=['POST'])
+def toggle_morse():
+    global mode
+    mode = Mode.MORSE
+    return redirect(url_for('index', mode=mode))
+
+
 def control_led():
     global mode
     try:
@@ -73,7 +97,7 @@ def control_led():
                 sleep(0.8)
                 pwm_led.value = 0
                 sleep(0.8)
-                for i in range (0,10):
+                for i in range(0, 10):
                     pwm_led.value = 1
                     sleep(0.2)
                     pwm_led.value = 0
@@ -86,10 +110,27 @@ def control_led():
                     pwm_led.value = 1 - (val * 0.1)
                     sleep(0.3)
                 sleep(1)
+            elif mode == Mode.MORSE:
+                morse_code = "MERRY CHRISTMAS"
+                for char in morse_code:
+                    if char in morse_dict:
+                        for symbol in morse_dict[char]:
+                            if symbol == '.':
+                                pwm_led.value = 1
+                                sleep(0.2)
+                            elif symbol == '-':
+                                pwm_led.value = 1
+                                sleep(0.6)
+                            elif symbol == '/':
+                                sleep(1.4)  # Space between words
+                            pwm_led.value = 0
+                            sleep(0.2)
+                        sleep(0.6)  # Space between letters
     except KeyboardInterrupt:
         pwm_led.close()
 
-# Avvio della funzione di controllo LED in un thread separato
+
+# Separate thread to control the LED
 led_thread = threading.Thread(target=control_led, daemon=True)
 led_thread.start()
 
